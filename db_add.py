63d38ff5fb93b0e31ecceeb605e1dc6709235dc6
Antoine Leclair <antoineleclair@gmail.com>
2024-02-28 21:16:28 -0500

    Fix imports

--- db_add.py
+++ db_add.py
@@ -5,10 +5,13 @@ from utils import (
     add_db,
     add_postgres_project,
     attach_db,
-    get_admin_conn_str,
     get_params_from_cli_args,
+)
+from storeutils import (
+    get_admin_conn_str,
     get_postgres_project_names,
 )
+
 
 def main():
     addon_project_name = os.environ.get("DISCO_PROJECT_NAME")
@@ -35,6 +38,7 @@ def main():
         api_key=api_key,
         disco_ip=disco_ip,
     )
+
 
 if __name__ == "__main__":
     main()
